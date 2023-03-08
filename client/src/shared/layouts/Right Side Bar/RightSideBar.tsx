becda6f4014e6ca919b6b99d6f290256eb946bfd
franz <francis.delossantos@sun-asterisk.com>
2023-03-08 18:26:54 +0800

    Fix comments

--- client/src/shared/layouts/Right Side Bar/RightSideBar.tsx
+++ client/src/shared/layouts/Right Side Bar/RightSideBar.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-interface rightSideBarProps {
+interface sideBarProps {
   children: any
 }
 
-const RightSideBar: React.FunctionComponent<rightSideBarProps> = ({
+export const RightSideBar: React.FunctionComponent<sideBarProps> = ({
   children
-}: rightSideBarProps) => {
+}: sideBarProps) => {
   return (
-    <div className='sidebar fixed top-0 bottom-0 right-20 bg-gray-400 w-80 mr-12 ml-6 '>
+    <div className="sidebar fixed top-0 bottom-0 right-20 bg-gray-400 w-80 mr-12 ml-6">
       {children}
     </div>
   );
 };
-
-export default RightSideBar;
