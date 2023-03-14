214bd12613b16b4490e61b446eaf495c73e56188
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2023-03-14 17:50:36 +0800

    Create ENV File for base URL

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
+  baseURL: process.env.NEXT_PUBLIC_ANALYTICS_API_BASE_URL
 });
 
 export default axiosInstance;
